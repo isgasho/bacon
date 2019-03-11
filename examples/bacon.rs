193ae88756dd30e2cc318a057e863516941e5bc1
aspera-non-spernit <g.s.d>
2019-03-11 22:01:26 +0100

    comment unused challenge

--- examples/bacon.rs
+++ examples/bacon.rs
@@ -7,7 +7,7 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-use bacon::{ Bacon, speck };
+use bacon::{ Bacon, speck };  // speck to use macros fry! unfry!
 use rand::{ distributions::{ Alphanumeric }, Rng };
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -21,26 +21,26 @@ struct Person {
     description: String
 }
 
-// a previously fried Person, password ungfortunately lost
-fn test_fried_bacon() -> Bacon {
-    let ukwn_fried_person = "{
-        \"data\": [
-            199989347279576730303273302831594482298,
-            89686176579382696547722799744440123650,
-            200571766683922312051532064628431861624,
-            272773095845529367494637394170706300494,
-            135123051543652942388160897815955354259,
-            241581523358705025852766085352301738865,
-            294742954104957886459273945034249829905,
-            170057818899735271015144833461180432525,
-            86971309882052014176151965736908291274
-        ]
-    }";
-    let person: Bacon = serde_json::from_str(ukwn_fried_person).unwrap();
-    person
-}
-
-// encrypts a struct using the speck algorithm and decrypts it back
+/* a previously fried Person, password ungfortunately lost
+    fn test_fried_bacon() -> Bacon {
+        let ukwn_fried_person = "{
+            \"data\": [
+                199989347279576730303273302831594482298,
+                89686176579382696547722799744440123650,
+                200571766683922312051532064628431861624,
+                272773095845529367494637394170706300494,
+                135123051543652942388160897815955354259,
+                241581523358705025852766085352301738865,
+                294742954104957886459273945034249829905,
+                170057818899735271015144833461180432525,
+                86971309882052014176151965736908291274
+            ]
+        }";
+        let person: Bacon = serde_json::from_str(ukwn_fried_person).unwrap();
+        person
+    }
+*/
+// encrypts a struct using the speck algorithm and decrypts it back using macros fry! and unfry!
 // $ cargo run --example bacon { optional 16 character pass } 
 fn main() {
     // key
