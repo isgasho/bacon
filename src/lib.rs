4fd6c45333aa4b371cbc30fac6c620f2782dbf30
aspera-non-spernit <g.s.d>
2019-03-12 21:26:17 +0100

    doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,47 @@
+//! The bacon crate provides functionality to en- and decrypt (called frying and unfrying),
+//! arbitrary ```struct T where T: Serialize + Deserialize```
+
 #[forbid(unsafe_code)]
 extern crate bincode;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 use serde::{ Deserialize, Serialize };
+use ciphers::speck::Speck;
+
 pub mod ciphers;
 
-/// ```Bacon``` a wrapper for an encrxypted struct in form of a ```Vec<u128>```
+/// ```Bacon``` a wrapper for an encrypted struct (called bacon) stored in the field ```data: Vec<u128>```
+/// Implements ```Fry``` and ```Unfry```. Cannot fry or unfry itself. (may change in the future).
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Bacon { pub data: Vec<u128> }
 /// Marker trait for Ciphers supported by Bacon
-pub trait Cipher {}  // to be implemented by Speck, ChaCha etc
+pub trait Cipher {} // to be implemented by Speck, ChaCha etc
 /// Provides methods to fry a struct. Returns a fried Bacon
 pub trait Fry { fn fry<T: Serialize>(source: T, key: u128) -> Bacon; }
 /// Provides methods to unfry a fried Bacon. Returns a bincode::Result<T>
 pub trait Unfry { fn unfry<U: Cipher, T: for<'de> Deserialize<'de>>(bacon: Bacon, key: u128) -> bincode::Result<T>; }
 
-/// A wrapper to support Fyring of Strings
+/// A wrapper to support Fyring of Strings. Can also be used to enrcypt messages from the command line.
+/// See example ```command_line.rs```
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Fryable { data: Vec<String> }
 
 /// Preferred way of creating a Fryable
-impl From<Vec<String>> for Fryable {
-    fn from(data:  Vec<String>) -> Self { Fryable { data } }
-}
-/// ```Bacon is the wrapper for all supported ciphers. It implements ```Fry``` and ```Unfry```.
+impl From<Vec<String>> for Fryable {  fn from(data:  Vec<String>) -> Self { Fryable { data } } }
+
 impl Fry for Bacon {
     fn fry<T: Serialize>(source: T, key: u128) -> Bacon {
         fry!(source, key)
     }
 }
-/// ```Bacon is the wrapper for all supported ciphers. It implements ```Fry``` and ```Unfry```.
 impl Unfry for Bacon {
     fn unfry<U: Cipher, T: for<'de> Deserialize<'de>>(bacon: Bacon, key: u128) -> bincode::Result<T> {
         unfry!(bacon, T, key)
     }
 }
 
-/// Utility function to turn a ```&str``` into a u128. The max lengths considered are 16 characters.
+/// Utility function to turn a ```&str``` into a u128. The max length considered is 16 characters.
 pub fn key_128(pass: &str) -> u128 {
     let mut x:  [u8; 16] = [0; 16];
      for (count, byte) in pass.as_bytes().iter().enumerate() {
@@ -45,12 +49,12 @@ pub fn key_128(pass: &str) -> u128 {
     }
     u128::from_be_bytes(x)
 }
-/// Macro to fry an arbitrary struct Does only support ```ciphers::Speck```
+/// Fry an arbitrary T: Serialize. Does only support ```ciphers::speck::Speck```
 #[macro_export]
 macro_rules! fry {
     ($item:ident, $key:ident) => {
         {
-            let key = ciphers::Speck::new($key);
+            let key = Speck::new($key);
             let byte_doc = bincode::serialize(&$item).unwrap();
             let chunks = byte_doc.chunks(16);
             drop($item);
@@ -69,12 +73,12 @@ macro_rules! fry {
     }
 }
 
-/// Macro to unfry an arbitrary struct. Does only support ```ciphers::Speck``` 
+/// Unfry an arbitrary T: Serialize. Does only support ```ciphers::speck::Speck``` 
 #[macro_export]
 macro_rules! unfry {
     ($fried_bacon:ident, $target:ty, $key:ident) => {
         {
-            let key = ciphers::Speck::new($key);
+            let key = Speck::new($key);
             let mut decr_bytes: Vec<u8> = vec![];
             for chunk in $fried_bacon.data {
                 for byte in u128::to_be_bytes(key.decrypt_block(chunk)).iter() {
