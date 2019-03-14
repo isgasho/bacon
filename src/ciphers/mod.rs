05388d2f50f7f388a5548bf483eab27ee77dcb96
aspera-non-spernit <g.s.d>
2019-03-14 22:35:35 +0100

    add trait Authenticate

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -2,15 +2,22 @@
 #![forbid(unsafe_code)]
 extern crate bigint;
 
+use std::hash::{ Hash, Hasher };
+
 pub mod speck;
 pub mod chacha20;
 
 use super::Bacon;
+#[derive(Debug)]
+pub struct MAC(u64);
 /// trait Cipher must be implemented by Ciphers.
 pub trait Cipher { 
     type Key;
     type Cipher;
     fn new(k: Self::Key, n: Option<[u8; 8]>) -> Self;
+}
+pub trait Authenticate : Hash {
+    fn hash(&self, bacon: Bacon) -> MAC;
 }
 // TODO: return Result<Bacon, BaconError<T> 
 pub trait Decrypt { fn decrypt(&self, bacon: Bacon) -> Bacon; }
